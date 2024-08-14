e4262088299d36948389151ed4aa33726129a163
khj980211 <khj980211@naver.com>
2024-08-14 20:11:13 +0900

    Intro video & building point added

--- Assets/Scripts/InformationBackground.cs
+++ Assets/Scripts/InformationBackground.cs
@@ -10,36 +10,68 @@ public class InformationBackground : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        string buildingName = PlayerPrefs.GetString("BuildingName");
+        switch(buildingName)
+        {
+            case "Cafe":
+                ChangeBackground(0);
+                break;
+            case "Bank":
+                ChangeBackground(2);
+                break;
+            // case "Home":
+            //     ChangeBackground(0);
+            //     break;
+            case "FireStation":
+                ChangeBackground(3);
+                break;
+            case "Hospital":
+                ChangeBackground(5);
+                break;
+            case "School":
+                ChangeBackground(13);
+                break;
+            case "Police":
+                ChangeBackground(11);
+                break;
+            case "Library":
+                ChangeBackground(7);
+                break;
+            case "Mart":
+                ChangeBackground(9);
+                break;
+        }
+
         if (backgroundRenderer == null)
         {
             backgroundRenderer = GetComponent<SpriteRenderer>();
         }    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            ChangeBackground(0);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            ChangeBackground(1);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            ChangeBackground(2);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            ChangeBackground(3);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            ChangeBackground(4);
-        }
-    }
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.Alpha1))
+    //     {
+    //         ChangeBackground(0);
+    //     }
+    //     else if (Input.GetKeyDown(KeyCode.Alpha2))
+    //     {
+    //         ChangeBackground(1);
+    //     }
+    //     else if (Input.GetKeyDown(KeyCode.Alpha3))
+    //     {
+    //         ChangeBackground(2);
+    //     }
+    //     else if (Input.GetKeyDown(KeyCode.Alpha4))
+    //     {
+    //         ChangeBackground(3);
+    //     }
+    //     else if (Input.GetKeyDown(KeyCode.Alpha5))
+    //     {
+    //         ChangeBackground(4);
+    //     }
+    // }
 
     public void ChangeBackground(int index)
     {
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class Player : MonoBehaviour
 {
+    private VideoPlayer videoPlayer;
+    public VideoClip[] videoClips;
     public float moveSpeed;  //이동속도
     Animator anim;
 
     void Start()
     {
         anim = GetComponent<Animator>();  //에니메이션 접근
+
+        //Video Player 컴포넌트 가져오기
+        if (videoPlayer == null)
+            videoPlayer = FindObjectOfType<VideoPlayer>();
+
+        if (videoPlayer != null)
+            videoPlayer.loopPointReached += EndReached;
+
     }
     
     void Update()
@@ -31,10 +43,45 @@ public class Player : MonoBehaviour
         transform.Translate(new Vector2(inputX,inputY) * Time.deltaTime * moveSpeed);
     }
 
+    //캐릭터가 특정건물 앞에 도착했을 때
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.tag == "Point") {
-            Debug.Log(other.gameObject.name);
-            SceneManager.LoadScene("Information");
+        if (other.gameObject.tag == "Point" && videoPlayer != null) 
+        {
+            switch (other.gameObject.name)
+            {
+                case "School":
+                    videoPlayer.clip = videoClips[2];
+                    break;
+                case "FireStation":
+                    videoPlayer.clip = videoClips[0];
+                    break;
+                // case "Library":
+                //     videoPlayer.clip = null;
+                //     break;
+                // case "Home":
+                //     videoPlayer.clip = null;
+                //     break;
+                // case "Mart":
+                //     videoPlayer.clip = null;
+                //     break;
+                case "Police":
+                    videoPlayer.clip = videoClips[1];
+                    break;
+                // case "Bank":
+                //     videoPlayer.clip = null;
+                //     break;
+                // case "Hospital":
+                //     videoPlayer.clip = null;
+                //     break;
+            }
+
+            videoPlayer.Play();
         }
+    }
+
+    void EndReached(VideoPlayer vp)
+    {
+        PlayerPrefs.SetString("BuildingName", vp.clip.name.Split("_")[0]);
+        SceneManager.LoadScene("Information");
     }
 }
