be92af9d2475617dbab26c24659371965e726164
khj980211 <khj980211@naver.com>
2024-09-15 15:03:41 +0900

    FireStation end game button added

---
+++ Assets/Scripts/FireStationGame1/EndGameButtonHandler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndGameButtonHandler : MonoBehaviour
+{
+    public Button endGameButton;
+    public void Start()
+    {
+        endGameButton.onClick.AddListener(EndGameButtonClick);
+    }
+
+    void EndGameButtonClick() 
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
