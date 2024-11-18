7840d3e617b6076929806191c87e33e6800a6a82
khj980211 <khj980211@naver.com>
2024-11-18 15:21:26 +0900

    SmartPhone hint added

--- Assets/Scripts/SmartPhoneManager.cs
+++ Assets/Scripts/SmartPhoneManager.cs
@@ -11,8 +11,10 @@ using System;
 public class SmartPhone : MonoBehaviour
 {
     public Button[] numberBtns;
-    public Button backspaceBtn;
+    public Button backspaceBtn, enterBtn, hintBtn;
+    public Image hintPopup;
     public TextMeshProUGUI phoneNumText;
+    public TextShake textShake;
 
     private DatabaseManager dbManager;
 
@@ -22,34 +24,59 @@ public class SmartPhone : MonoBehaviour
     private bool isComplate = false;
     void Start()
     {
+        //DB 연결
         dbManager = new DatabaseManager();
         dbManager.Connect();
+        
+        //현재 접속한 사용자의 부모님 전화번호 가져옴
         string parentPhoneNumber = dbManager.getParentPhoneNumber(1);
+
+        //힌트버튼 숨김
+        hintBtn.gameObject.SetActive(false);
+
+        //DB에 저장된 전화번호는 '-'이 없어 추가해서 hint text에 세팅
+        TextMeshProUGUI hintPhoneNumber = hintPopup.GetComponentInChildren<TextMeshProUGUI>();
+        string hintParentPhoneNum = parentPhoneNumber.Insert(3, "-").Insert(8, "-");
+        hintPhoneNumber.text = hintParentPhoneNum;
+
+        //힌트(부모님 전화번호) 숨김
+        hintPopup.gameObject.SetActive(false);
 
         foreach (Button btn in numberBtns) 
         {
             btn.onClick.AddListener(() => {
                 if (phoneNumText.text.Length != 13) phoneNumText.text += (phoneNumText.text.Length == 3 || phoneNumText.text.Length == 8) ? $"-{btn.name}" : btn.name;
-                
-                string inputted = Regex.Replace(phoneNumText.text, @"\D", ""); // 숫자만 남기고 다 제거
-                parentPhoneNumber = Regex.Replace(parentPhoneNumber, @"\D", ""); // 숫자만 남기고 다 제거
-
-                if (inputted == parentPhoneNumber) 
-                {
-                    isComplate = true;
-
-                    //gameObject.SetActive(false);
-                    foreach (Button button in numberBtns) 
-                    {
-                        button.onClick.RemoveAllListeners();
-                        backspaceBtn.onClick.RemoveAllListeners();
-                    }
-                }
             });
         }
 
         backspaceBtn.onClick.AddListener(() => {
             if (phoneNumText.text.Length > 0) phoneNumText.text = phoneNumText.text.Remove(phoneNumText.text.Length - ((phoneNumText.text.Length == 10 || phoneNumText.text.Length == 5) ? 2 : 1));
+        });
+
+        enterBtn.onClick.AddListener(() => {
+            string inputted = Regex.Replace(phoneNumText.text, @"\D", ""); // 숫자만 남기고 다 제거
+            parentPhoneNumber = Regex.Replace(parentPhoneNumber, @"\D", ""); // 숫자만 남기고 다 제거
+
+            if (inputted == parentPhoneNumber) 
+            {
+                isComplate = true; //스마트폰 오브젝트 이동할 때 사용
+                
+                foreach (Button button in numberBtns) 
+                {
+                    //사용자가 입력한 번호가 일치하면 버튼들 리스너 제거해서 작동 안하도록 처리
+                    button.onClick.RemoveAllListeners();
+                    backspaceBtn.onClick.RemoveAllListeners();
+                }
+            }
+            else 
+            {
+                textShake.ShakeText();
+                hintBtn.gameObject.SetActive(true); //사용자가 입력한 전화번호가 틀렸을 경우 힌트버튼 가시화
+            }
+        });
+
+        hintBtn.onClick.AddListener(() => {
+            hintPopup.gameObject.SetActive(!hintPopup.isActiveAndEnabled);
         });
     }
 
