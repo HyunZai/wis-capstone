7ff73183cf47d12d4c1023c9c7f78bc4c80ee70e
Dahye <dahye00103@gmail.com>
2024-11-20 23:46:27 +0900

    수정
    1. 버튼 클릭 소리 추가
    2. 카페 게임 material속도 줄임

--- Assets/Scripts/Cafe/CafeMaterial.cs
+++ Assets/Scripts/Cafe/CafeMaterial.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class CafeMaterial : MonoBehaviour
 {
     [SerializeField]
-    private float moveSpeed = -0.05f;
+    private float moveSpeed = 0.05f;
     private float minY = -7;
     // Update is called once per frame
     void Update()
     {
+        Debug.Log($"Current moveSpeed: {moveSpeed}");
         transform.position += Vector3.down * moveSpeed * Time.deltaTime;
         if(transform.position.y < minY) {
             Destroy(gameObject);   // 재료가 사라짐
